fd5b22d8ea4007565cb0e92ef58b2d5e6253720a
Dima <75967292+DukaDima@users.noreply.github.com>
2021-08-30 23:06:32 +0300

    рендеринг первой страницы

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -19,11 +19,11 @@ export default class ImageGallery extends Component {
   };
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.searchPhoto !== this.props.searchPhoto) {
-      this.setState({ page: 1 });
-      this.setState({ photos: [] });
-      this.setState({ loading: true });
+      this.setState({ page: 1, photos: [], loading: true });
+      // this.setState({ photos: [] });
+      // this.setState({ loading: true });
       fetch(
-        `https://pixabay.com/api/?q=${this.props.searchPhoto}&page=${this.state.page}&key=22333452-088c943be01bb3bdea991b2c2&image_type=photo&orientation=horizontal&per_page=12`,
+        `https://pixabay.com/api/?q=${this.props.searchPhoto}&page=1&key=22333452-088c943be01bb3bdea991b2c2&image_type=photo&orientation=horizontal&per_page=12`,
       )
         .then(res => res.json())
         .then(photos => this.setState({ photos: photos.hits }))
