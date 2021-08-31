08370a721bc307697087482bef4d54e55803c785
Dima <75967292+DukaDima@users.noreply.github.com>
2021-08-31 08:44:34 +0300

    деструкторизация пропсов и стейтов

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -10,6 +10,7 @@ export default class ImageGallery extends Component {
     loading: PropTypes.bool,
     page: PropTypes.number,
     error: PropTypes.any,
+    searchPhoto: PropTypes.string,
   };
   state = {
     photos: [],
@@ -18,12 +19,12 @@ export default class ImageGallery extends Component {
     error: null,
   };
   componentDidUpdate(prevProps, prevState) {
-    if (prevProps.searchPhoto !== this.props.searchPhoto) {
+    const { searchPhoto } = this.props;
+    const { page } = this.state;
+    if (prevProps.searchPhoto !== searchPhoto) {
       this.setState({ page: 1, photos: [], loading: true });
-      // this.setState({ photos: [] });
-      // this.setState({ loading: true });
       fetch(
-        `https://pixabay.com/api/?q=${this.props.searchPhoto}&page=1&key=22333452-088c943be01bb3bdea991b2c2&image_type=photo&orientation=horizontal&per_page=12`,
+        `https://pixabay.com/api/?q=${searchPhoto}&page=1&key=22333452-088c943be01bb3bdea991b2c2&image_type=photo&orientation=horizontal&per_page=12`,
       )
         .then(res => res.json())
         .then(photos => this.setState({ photos: photos.hits }))
@@ -31,13 +32,13 @@ export default class ImageGallery extends Component {
         .finally(() => this.setState({ loading: false }));
     }
     if (
-      prevState.page !== this.state.page &&
-      this.state.page > 1 &&
-      prevProps.searchPhoto === this.props.searchPhoto
+      prevState.page !== page &&
+      page > 1 &&
+      prevProps.searchPhoto === searchPhoto
     ) {
       this.setState({ loading: true });
       fetch(
-        `https://pixabay.com/api/?q=${this.props.searchPhoto}&page=${this.state.page}&key=22333452-088c943be01bb3bdea991b2c2&image_type=photo&orientation=horizontal&per_page=12`,
+        `https://pixabay.com/api/?q=${searchPhoto}&page=${page}&key=22333452-088c943be01bb3bdea991b2c2&image_type=photo&orientation=horizontal&per_page=12`,
       )
         .then(res => res.json())
         .then(photos =>
